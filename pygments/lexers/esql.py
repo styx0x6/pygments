af12dcf90e17b8596c522e7ce5b4841ac3ff2ae1
Styx0x6 <styx0x6@users.noreply.github.com>
2026-09-07 17:57:32 +0200

    Next release is now 2.22

--- pygments/lexers/esql.py
+++ pygments/lexers/esql.py
@@ -100,7 +100,7 @@ class ESQLLexer(RegexLexer):
     filenames = ["*.esql"] # Not official, we do not add any *.es extension here as it would overlap erlang.ErlangLexer
     mimetypes = []
     url = "https://www.elastic.co/guide/en/elasticsearch/reference/current/esql.html"
-    version_added = '2.21'
+    version_added = '2.22'
 
     flags = re.IGNORECASE
 
